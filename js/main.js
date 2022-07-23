85ee825f5d31c5de0828a280cc614484954f6d68
xasan <xasandj2002@gmail.com>
2022-07-23 18:53:30 +0500

    http

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var a = +prompt("Son kriting!");
+
+if (a % 2 == 0 && !isNaN(a)) {
+  alert("Juft son krittingiz!");
+} else if (a % 2 == 1 && !isNaN(a)) {
+  alert("Toq son krittingiz!");
+} else {
+  alert("Bu son emas iltimos son kriting!");
+}
